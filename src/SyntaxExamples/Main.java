cda00d1bd0683b214e63e5e3f96780d0e05b3bb3
eggliot <eharhar06@proton.me>
2024-10-10 16:37:55 -0700

    add switch statement example vs else if statements

--- src/SyntaxExamples/Main.java
+++ src/SyntaxExamples/Main.java
@@ -204,6 +204,63 @@ public class Main {
             }
         }
         
+        
+        // Switch Statements
+        int day = 4;
+        // Instead of writing this:
+        if (day == 1) {
+            System.out.println("Sunday");
+        }
+        else if (day == 2) {
+            System.out.println("Monday");
+        }
+        else if (day == 3) {
+            System.out.println("Tuesday");
+        }
+        else if (day == 4) {
+            System.out.println("Wednesday");
+        }
+        else if (day == 5) {
+            System.out.println("Thursday");
+        }
+        else if (day == 6) {
+            System.out.println("Friday");
+        }
+        else if (day == 7) {
+            System.out.println("Saturday");
+        }
+        else {
+            System.out.println("Invalid day");
+        }
+        // Write this:
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+            
+        
 
         
         
